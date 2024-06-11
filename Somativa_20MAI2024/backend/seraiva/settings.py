280b87c9a332df3ef12b448b8fe673429eaaa08b
Rodrinardi <125322284+Rodrinardi@users.noreply.github.com>
2024-06-11 19:47:46 -0300

    deploy backend

--- Somativa_20MAI2024/backend/seraiva/settings.py
+++ Somativa_20MAI2024/backend/seraiva/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nilp7m4+ssq-(&71rz$%w&=bvif92p(zgk-dklax@2180+1b=('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,7 +93,7 @@ WSGI_APPLICATION = 'seraiva.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': env('ENGINE'),
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
